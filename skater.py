a17254789160ee210489265b028aa2d7d3db19c0
onjas <onjas.buidl@gmail.com>
2023-08-05 04:08:15 -0700

    update detect_openai logic & file-to-update search logic

--- skater.py
+++ skater.py
@@ -17,7 +17,6 @@ else:
     base_dir = os.getcwd()
     target_repo_dir = os.path.join(base_dir, "repo_skated", repo_name)
 
-# 3. Change the working directory to the local directory and Pull the latest codebase from GitHub
 os.chdir(target_repo_dir)
 repo = Repo(target_repo_dir)
 
@@ -32,27 +31,19 @@ def extract_code_segments(text):
 
 
 def detect_openai_api(code_segment):
-    if "openai" in code_segment.lower():
+    if "openai" in code_segment:
         return True
     else:
         return False
 
 
-def update_file_with_llm(file_path):
+def update_file_with_llm(file_content):
     """
 
     :param file_path:
     :return -> bool: whether the file is updated.
     """
     # Load the codebase into a variable
-    with open(file_path, 'r') as file:
-        file_content = file.read()
-
-    if not detect_openai_api(file_content):
-        return False
-
-    print("Updating the file: {}".format(file_path))
-
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature=0,
@@ -147,34 +138,24 @@ Both code snippets assume that there is an OPENAI_API_KEY environmental variable
     return True
 
 
-# 4. Load all .py files in local_dir separately and update the codebase with the new code
-for root, dirs, files in tqdm.tqdm(os.walk(target_repo_dir)):
-    # print(root, '===', dirs,'===', files)
-    for file in files:
-        if file.endswith('.py'):
-            file_path = os.path.join(root, file)
-            is_updated = update_file_with_llm(file_path)
-            if is_updated:
-                print("Updated file: {}".format(file_path))
-
-# 5. Commit the changes with a relevant message and push the changes to Github
-# repo.git.add(update=True)
-# repo.index.commit("Skateboard: Update codebase to LangChain.")
-#
-# import random
-#
-# # Generate a random integer between 0 and 999999
-# number = random.randint(0, 999999)
-#
-# # Convert to string and pad with zeroes if necessary
-# random_string = str(number).zfill(6)
-#
-# new_branch_name = 'Skateboard-LangChain_update'
-#
-#
-# # Create a new branch
-# repo.git.checkout('HEAD', b=new_branch_name)
-# # Push the new branch to the remote repository
-# repo.git.push('--set-upstream', 'origin', new_branch_name)
-#
-# origin.push()
+AUTOMATIC_SEARCH_FILE_TO_UPDATE = False
+if AUTOMATIC_SEARCH_FILE_TO_UPDATE:
+    for root, dirs, files in tqdm.tqdm(os.walk(target_repo_dir)):
+        # print(root, '===', dirs,'===', files)
+        for file in files:
+            if not file.endswith('.py'):
+                continue
+            with open(os.path.join(root, file), 'r') as file:
+                file_content = file.read()
+            if detect_openai_api(file_content):
+                print("Found OpenAI API in file: {}".format(file))
+                update_file_with_llm(file_content)
+                print("Updated file: {}".format(file))
+else:
+    files_paths_to_update = ['/Users/jasonhu/Desktop/aider/aider/sendchat.py',
+                             '/Users/jasonhu/Desktop/aider/aider/main.py']
+    for file_path in tqdm.tqdm(files_paths_to_update):
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+        update_file_with_llm(file_content)
+        print("Updated file: {}".format(file_path))
